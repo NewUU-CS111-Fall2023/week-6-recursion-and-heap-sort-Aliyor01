0e97b9ba931f7cbd923d9d29ccdf3bc25da3209d
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-23 23:16:50 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,29 @@
 /*
- * Author:
- * Date:
+ * Author:  Ergashev Aliyor
+ * Date: 23.11.2023
  * Name:
  */
+#include <iostream>
+using namespace std;
+
+void printSequence(int start, int end, int step) {
+    if (start == end) {
+        cout << start << " ";
+        return;
+    }
+    cout << start << " ";
+    printSequence(start + step, end, step);
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    if (x > y) {
+        printSequence(x, y, -1); // Print in descending order
+    } else {
+        printSequence(x, y, 1);  // Print in ascending order
+    }
+
+    return 0;
+}
