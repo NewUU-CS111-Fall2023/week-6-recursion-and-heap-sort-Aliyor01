b62cb4731ecde61fb38323eb566afd6a54cc9883
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-23 23:22:30 +0500

    Update task_6.h

--- src/task_6.h
+++ src/task_6.h
@@ -1,1 +1,41 @@
+/*
+ * Author:  Ergashev Aliyor
+ * Date: 23.11.2023
+ * Name:
+ */
+#include <iostream>
+#include <climits>
+using namespace std;
 
+void findSecondLargest(int& largest, int& secondLargest) {
+    int num;
+    cin >> num;
+
+    if (num == 0) {
+        return;
+    }
+
+    if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+    } else if (num > secondLargest && num != largest) {
+        secondLargest = num;
+    }
+
+    findSecondLargest(largest, secondLargest);
+}
+
+int main() {
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    findSecondLargest(largest, secondLargest);
+
+    if (secondLargest != INT_MIN) {
+        cout << secondLargest << endl;
+    } else {
+        cout << "There is no second largest element." << endl;
+    }
+
+    return 0;
+}
